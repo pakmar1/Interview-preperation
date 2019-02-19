0a0b7e414c8be7bffb4094c72f10e5aeb5bef838
Prasad Akmar <31735926+pakmar1@users.noreply.github.com>
2019-02-19 18:47:17 -0500

    problem 448

---
+++ Leetcode/find_app_disappeared_numbers.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: 'List[int]') -> 'List[int]':
+        ret = []
+        for i in range(len(nums)):
+            index = abs(nums[i]) - 1
+            if nums[index] > 0:
+                nums[index] = -nums[index]
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                ret.append(i + 1)
+        return ret
