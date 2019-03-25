814403ce6e79357575c5794c2f64d95455b5759a
Prasad Akmar <31735926+pakmar1@users.noreply.github.com>
2019-03-25 16:49:43 -0400

    problem 485

---
+++ Leetcode/max_consecutive_ones.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        if(nums.length==0) return 0;
+        int count=0;
+        int maxCount=0;
+        for(int i=0;i<nums.length;i++){
+            count++;
+            if(nums[i]==0){
+                count=0;
+            }
+            if(count>maxCount){
+                maxCount=count;
+            }
+            
+        }
+        return maxCount;
+    }
+}
