43edad01b6d37da1c91f40493474a6a8b7a54823
Prasad Akmar <31735926+pakmar1@users.noreply.github.com>
2019-03-27 15:53:05 -0400

    problem 212

---
+++ Leetcode/word_search_II.java
@@ -0,0 +1,57 @@
+class Solution {
+    
+    public List<String> findWords(char[][] board, String[] words) {
+        for(String word:words) insert(word);
+        int row = board.length;
+        int col = board[0].length;
+        boolean[][] visited = new boolean[row][col];
+        for(int i=0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                TrieNode t=root;
+                if(t.children[board[i][j]-'a']==null) continue;
+                else{
+                    find(board,i,j,row,col,t,visited);
+                }
+            }
+        }
+        return new ArrayList<String>(res);
+    }
+    
+    class TrieNode{
+        TrieNode[] children;
+        String word;
+        TrieNode(){
+            children=new TrieNode[26];
+        }
+    }
+    
+    TrieNode root=new TrieNode();
+    Set<String> res=new HashSet<>();
+    public void insert(String word){
+        TrieNode t = root;
+        for(char c:word.toCharArray()){
+            if(t.children[c-'a']==null){
+                t.children[c-'a']=new TrieNode();
+            }
+            t = t.children[c-'a'];
+        }
+        t.word = word;
+    }
+    public void find(char[][] board,int i,int j,int row,int col,TrieNode t,boolean[][] visited){
+        if(i >= 0 &&i < row && j >= 0 && j < col){
+            if(!visited[i][j]){
+                if(t.children[board[i][j]-'a'] != null){
+                    visited[i][j] = true;
+                    if(t.children[board[i][j]-'a'].word != null) res.add(t.children[board[i][j]-'a'].word);
+                    find(board,i-1,j,row,col,t.children[board[i][j]-'a'],visited);
+                    find(board,i+1,j,row,col,t.children[board[i][j]-'a'],visited);
+                    find(board,i,j-1,row,col,t.children[board[i][j]-'a'],visited);
+                    find(board,i,j+1,row,col,t.children[board[i][j]-'a'],visited);
+                    visited[i][j] = false;
+                }
+            }
+        }
+    }
+    
+    
+}
