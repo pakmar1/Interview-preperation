f3ce198eb3c92fd8cf7506f63b06daa3d54fd5cf
Prasad Akmar <31735926+pakmar1@users.noreply.github.com>
2019-03-28 11:38:03 -0400

    problem 317

---
+++ Leetcode/find_k_smallest_pair_distance.java
@@ -0,0 +1,20 @@
+class Solution {
+     public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int low = 0;
+        int high = nums[nums.length - 1] - nums[0];
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int count = 0, left = 0;
+            for (int right = 0; right < nums.length; ++right) {
+                while (nums[right] - nums[left] > mid) left++;
+                count += right - left;
+            }
+            //count = number of pairs with distance <= mi
+            if (count >= k) high = mid;
+            else low = mid + 1;
+        }
+        return low;
+    }
+}
