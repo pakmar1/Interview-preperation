d766b37ada49fc282ed18f67d39a625163a22e4c
Prasad Akmar <31735926+pakmar1@users.noreply.github.com>
2019-04-09 10:21:04 -0400

    problem 344
    added recursive solution

--- Leetcode/reverse_a_string.java
+++ Leetcode/reverse_a_string.java
@@ -12,4 +12,21 @@ class Solution {
         }
         return sBuffer.toString();
     }
+
+
+
+    public void reverseStringRecursive(char[] s) {
+        int start=0,end=s.length-1;
+        reverseRecurse(s,start,end);     
+    }
+    
+    public void reverseRecurse(char[] s, int start, int end) {
+        if(start<end) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            reverseRecurse(s,start+1,end-1);
+        }
+    }
+    
 }
