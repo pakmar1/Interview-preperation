ca284b40d3c2ed50942cfa20f595682efdbb04c1
Prasad Akmar <31735926+pakmar1@users.noreply.github.com>
2019-04-10 12:31:52 -0400

    problem 112
    recursive approach

---
+++ Leetcode/path_sum.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int addition;
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        sum -= root.val;
+        if (sum == 0 && (root.left == null && root.right ==null)) return true;
+        return hasPathSum(root.left, sum) || hasPathSum(root.right,sum);
+    }
+}
