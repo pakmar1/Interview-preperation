cfe99bd3e861b4d0527b4d921a4ebf1aa979b0f2
Prasad Akmar <31735926+pakmar1@users.noreply.github.com>
2019-04-10 11:40:41 -0400

    problem 102

--- Leetcode/binary_tree_level_order_traversal.java
+++ Leetcode/binary_tree_level_order_traversal.java
@@ -28,3 +28,34 @@ class Solution {
         compute(ans,curr.right,level+1);
     }
 }
+***********************************************************************************************/**
+ * non-recursive solution using Queues
+ * 
+ */
+class Solution1 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if (root != null) {
+            q.add(root);
+        }
+        TreeNode cur;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> subAns = new LinkedList<Integer>();
+            for (int i = 0; i < size; ++i) {        // traverse nodes in the same level
+                cur = q.remove();
+                subAns.add(cur.val);                // visit the root
+                if (cur.left != null) {
+                    q.add(cur.left);              // push left child to queue if it is not null
+                }
+                if (cur.right != null) {
+                    q.add(cur.right);             // push right child to queue if it is not null
+                }
+            }
+            ans.add(subAns);
+        }
+        return ans;
+    }
+}
