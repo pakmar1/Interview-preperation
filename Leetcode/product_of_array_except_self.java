1f449854ef8cf153356b8396246e822b502c4e5c
Prasad Akmar <31735926+pakmar1@users.noreply.github.com>
2019-04-17 14:50:35 -0400

    product 238

---
+++ Leetcode/product_of_array_except_self.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] output = new int[len];
+        int product = 1;
+        for(int i=0;i<len;i++){
+            output[i] = product;
+            product *= nums[i];
+        }
+        product = 1;
+        for(int i=len-1;i>=0;i--){
+            output[i] *= product;
+            product *= nums[i];
+        }
+        return output;
+    }
+}
