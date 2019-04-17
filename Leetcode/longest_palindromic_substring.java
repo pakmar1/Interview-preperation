868816cdd609f0abd08d05478be1b0dc3ce45b63
Prasad Akmar <31735926+pakmar1@users.noreply.github.com>
2019-04-17 14:21:13 -0400

    problem 5

--- Leetcode/longest_palindromic_substring.java
+++ Leetcode/longest_palindromic_substring.java
@@ -4,9 +4,8 @@ class Solution {
         if (s == null || s.length() < 1) return "";
         int start = 0, end = 0;
         for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);
-            int len2 = expandAroundCenter(s, i, i + 1);
-            int len = Math.max(len1, len2);
+            
+            int len = Math.max(expand(s, i, i), expand(s, i, i+1));
             if (len > end - start) {
                 start = i - (len - 1) / 2;
                 end = i + len / 2;
@@ -15,12 +14,12 @@ class Solution {
         return s.substring(start, end + 1);
     }
 
-    private int expandAroundCenter(String s, int left, int right) {
-        int L = left, R = right;
-        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
-            L--;
-            R++;
+    private int expand(String s, int left, int right) {
+        
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
         }
-        return R - L - 1;
+        return right - left - 1;
     }
 }
