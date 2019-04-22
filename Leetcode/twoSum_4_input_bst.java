ef948f63f7e0403021a034984da5be860d334c20
Prasad Akmar <31735926+pakmar1@users.noreply.github.com>
2019-04-22 10:00:33 -0400

    problem 653
    TwoSum problem with input as a BST

---
+++ Leetcode/twoSum_4_input_bst.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    Set<Integer> set = new HashSet<>();
+    public boolean findTarget(TreeNode root, int k) {
+        if(root==null) return false;
+        else if(set.contains(root.val))return true;
+        set.add(k-root.val);
+        return findTarget(root.left,k) || findTarget(root.right,k);
+    }
+}
